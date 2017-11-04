f128ee19fcc32e2681e2f7a6ca17c565a6c1684a
Agon G <agonxgashi@gmail.com>
2017-11-04 15:51:32 +0100

    JWT Added

---
+++ time4movies.UI/Auth/TokenGenerator.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace time4movies.UI.Auth
+{
+    public class TokenGenerator
+    {
+        //TODO: Put these values in webconfig
+        public static readonly string TOKEN_SECRET = "t1m34m@v13s_DEVAPP_secretKey1234";
+        public static readonly string ISSUER       = "www.time4movies.com";
+        public static readonly string AUDIENCE     = "time4movies.com";
+
+
+        public static string Build()
+        {
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Acr, ""),
+                //new Claim(JwtRegisteredClaimNames.UniqueName, "agonxgashi"), //Username
+                //new Claim(JwtRegisteredClaimNames.Email, "agonxgashi@gmail.com"), //User Email
+                //new Claim(ClaimTypes.Role, "Admin"), //TODO: Role must be updated for each user
+                new Claim(JwtRegisteredClaimNames.Iat,
+                    DateTimeToUnixTimestamp(DateTime.Now)
+                    .ToString(CultureInfo.InvariantCulture),
+                    ClaimValueTypes.Integer64)
+            };
+
+            var token = new JwtSecurityToken(
+                              issuer            : ISSUER,
+                              audience          : AUDIENCE,
+                              claims            : claims,
+                              expires           : DateTime.UtcNow.AddDays(1000),
+                              signingCredentials: new SigningCredentials(Create(TOKEN_SECRET),
+                                                    SecurityAlgorithms.HmacSha256));
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public static double DateTimeToUnixTimestamp(DateTime dateTime)
+        {
+            return (dateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds;
+        }
+
+        public static SymmetricSecurityKey Create(string secret)
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
+        }
+    }
+}
--- time4movies.UI/Startup.cs
+++ time4movies.UI/Startup.cs
@@ -1,19 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Threading.Tasks;
+using time4movies.UI.Auth;
 
 namespace time4movies.UI
-{ 
+{
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        {
+        { 
             Configuration = configuration;
         }
 
@@ -22,6 +22,34 @@ namespace time4movies.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => {
+                        options.TokenValidationParameters =
+                             new TokenValidationParameters
+                             {
+                                 ValidateIssuer           = true,
+                                 ValidateAudience         = true,
+                                 ValidateLifetime         = true,
+                                 ValidateIssuerSigningKey = true,
+                                 ValidIssuer              = TokenGenerator.ISSUER,
+                                 ValidAudience            = TokenGenerator.AUDIENCE,
+                                 IssuerSigningKey         = TokenGenerator.Create(TokenGenerator.TOKEN_SECRET)
+                             };
+
+                        options.Events = new JwtBearerEvents
+                        {
+                            OnAuthenticationFailed = context =>
+                            {
+                                return Task.CompletedTask;
+                            },
+                            OnTokenValidated = context =>
+                            {
+                                return Task.CompletedTask;
+                            }
+                        };
+                    });
+
+
             services.AddMvc();
         }
 
@@ -42,6 +70,9 @@ namespace time4movies.UI
             }
 
             app.UseStaticFiles();
+
+            //This implements Authentication
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
