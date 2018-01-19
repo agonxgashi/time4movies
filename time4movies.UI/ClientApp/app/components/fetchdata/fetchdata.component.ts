e36e1f8288d7ccef2c98e05a710e75e5a8024aa4
driart gajtani <driart300@hotmail.com>
2018-01-19 20:44:54 +0100

    remove things

--- time4movies.UI/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ time4movies.UI/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -8,11 +8,11 @@ import { Http } from '@angular/http';
 export class FetchDataComponent {
     public forecasts: WeatherForecast[];
 
-    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-        http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-            this.forecasts = result.json() as WeatherForecast[];
-        }, error => console.error(error));
-    }
+    //constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+    //    http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+    //        this.forecasts = result.json() as WeatherForecast[];
+    //    }, error => console.error(error));
+    //}
 }
 
 interface WeatherForecast {
--- time4movies.UI/ClientApp/app/components/home/home.component.ts
+++ time4movies.UI/ClientApp/app/components/home/home.component.ts
@@ -30,7 +30,6 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
         this.getTrendinMovies();
-        //this.getRandomQuote();
     }
     searchMovies() {
         this.http.get("/api/Search/ByName" + this.queryUrl + this.name )    
@@ -41,14 +40,6 @@ export class HomeComponent implements OnInit {
         )
         
     }
-    //getRandomQuote() {
-    //    this.http.get("/api/Quote/RandomQuote")
-    //        .subscribe(
-    //        (res) => { this.quote = res.json(); console.log(res.json()); console.log(this.quote) },
-    //        (err) => { }
-    //        )
-
-    //}
 
     getTrendinMovies() {
         this.http.get("/api/Search/Trending")
