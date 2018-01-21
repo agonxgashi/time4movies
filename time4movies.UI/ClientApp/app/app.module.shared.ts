d3b122312052a37742fed54780e1ab544415573b
Agon G <agonxgashi@gmail.com>
2018-01-21 14:04:20 +0100

    Remove of some unused components [landing, fetch-data, sidebar]

--- time4movies.UI/ClientApp/app/app.module.shared.ts
+++ time4movies.UI/ClientApp/app/app.module.shared.ts
@@ -1,20 +1,16 @@
+import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
+import { CommonModule }         from '@angular/common';
+import { RouterModule }         from '@angular/router';
+import { AppComponent }         from './components/app/app.component';
+import { NavMenuComponent }     from './components/navmenu/navmenu.component';
+import { HomeComponent }        from './components/home/home.component';
+import { ProfileComponent }     from './components/profile/profile.component';
+import { MovieComponent }       from './components/movie/movie.component';
+import { LogInSrv }             from './services/logInService'
+import { httpFactory }          from './services/AuthServices/HttpFactory'
+import { NgModule }             from '@angular/core';
+import { FormsModule }          from '@angular/forms';
 
-import { LandingPageComponent } from './components/landing/landingpage.component';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { MovieComponent } from './components/movie/movie.component';
-import { LogInSrv } from './services/logInService'
-import { SideBarComponent } from './components/sidebar/sidebar.component';
-import { httpFactory } from './services/AuthServices/HttpFactory'
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {HttpModule, Http, XHRBackend, RequestOptions} from '@angular/http';
 
 
 @NgModule({
@@ -22,11 +18,8 @@ import {HttpModule, Http, XHRBackend, RequestOptions} from '@angular/http';
                 AppComponent,
                 NavMenuComponent,
                 ProfileComponent,
-                FetchDataComponent,
                 HomeComponent,
-                LandingPageComponent,
-                MovieComponent,
-                SideBarComponent
+                MovieComponent
 
             ],
             imports: [
@@ -35,12 +28,9 @@ import {HttpModule, Http, XHRBackend, RequestOptions} from '@angular/http';
                         FormsModule,
                         RouterModule.forRoot([
                             { path: '', redirectTo: 'home', pathMatch: 'full' },
-                            { path: 'home', component: HomeComponent },
-                            { path: 'profile', component: ProfileComponent },
-                            { path: 'fetch-data', component: FetchDataComponent },
-                            //{ path: 'landing', component: LandingPageComponent },
+                            { path: 'home',      component: HomeComponent },
+                            { path: 'profile',   component: ProfileComponent },
                             { path: 'movie/:id', component: MovieComponent },
-                            //{ path: 'sidebar', component: SideBarComponent },
                             { path: '**', redirectTo: 'home' }
                         ])
                     ],
